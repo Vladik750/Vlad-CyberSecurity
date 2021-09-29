08e657876eaeb000362c6fd51e1d96ca254a6ee8
Vlad Zhuk <zhuk.vlad2000@gmail.com>
2021-09-29 13:32:09 +0300

    Reverse string

---
+++ ReverseString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace ReverseString
+{
+    class Program
+    {
+        private static string ReverseString(string inputString)
+        {
+            if (inputString.Length == 0 || inputString == null)
+            {
+                return "Input error";
+            }
+            else
+            {
+                string temp = "";
+                for (int i = inputString.Length - 1; i > -1; i--)
+                {
+                    temp += inputString[i];
+                }
+                return temp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string reverseTo = "reverse"; //esrever
+            Console.WriteLine(ReverseString(reverseTo));
+        }
+    }
+}
